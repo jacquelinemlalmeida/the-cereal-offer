9c1c0a5c7c5fa4bbeaf2ec1ed6bc42e399ae67f7
Jacqueline M L Almeida <jackellita@hotmail.com>
2023-05-07 20:14:30 -0300

    removed config to use api with ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.hosts << "aa0e-187-5-24-113.ngrok-free.app"
 end
